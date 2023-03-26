525fa687517bd3676bcf9bc240deb64ef01d9ca1
Phạm Văn Trường <71928711+truongpham262000@users.noreply.github.com>
2023-03-26 17:24:48 +0700

    Delete FashionShop_FE directory
    delete

--- FashionShop_FE/src/environments/environment.prod.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @license
- * Copyright Akveo. All Rights Reserved.
- * Licensed under the MIT License. See License.txt in the project root for license information.
- */
-export const environment = {
-  production: true,
-  API_BASE_URL: "https://localhost:44352"
-};
