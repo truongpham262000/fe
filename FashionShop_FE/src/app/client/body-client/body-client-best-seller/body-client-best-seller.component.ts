1337e05bde15167b4b90c4d2cddbd42bd97074d3
truongpham262000 <pvtruong.12b3lqd2018@gmail.com>
2023-04-07 23:07:53 +0700

    no message

--- FashionShop_FE/src/app/client/body-client/body-client-best-seller/body-client-best-seller.component.ts
+++ FashionShop_FE/src/app/client/body-client/body-client-best-seller/body-client-best-seller.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../../../@core/data/FashionShopApi.service';
+import { BodyClientService } from '../body-client.service';
 
 @Component({
   selector: 'ngx-body-client-best-seller',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BodyClientBestSellerComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _service: BodyClientService) { }
+  target: Product[] = [];
 
   ngOnInit(): void {
+    this._service.selectProduct().subscribe(res => {
+      this.target = res;
+    })
   }
 
 }
--- FashionShop_FE/src/app/client/body-client/body-client.service.ts
+++ FashionShop_FE/src/app/client/body-client/body-client.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Client, Product } from '../../@core/data/FashionShopApi.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BodyClientService {
 
-  constructor() { }
+  constructor(private _api: Client) { }
+
+  selectProduct(): Observable<Product[]> {
+    return this._api.productsAll();
+  }
 }
