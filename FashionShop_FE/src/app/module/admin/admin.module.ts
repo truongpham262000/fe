4fb876a5caa51b5f8fb4a3464af014c82de069ac
truongpham262000 <pvtruong.12b3lqd2018@gmail.com>
2023-04-14 18:48:20 +0700

    update

--- FashionShop_FE/src/app/module/admin/admin.module.ts
+++ FashionShop_FE/src/app/module/admin/admin.module.ts
@@ -2,20 +2,24 @@ import { NgModule } from '@angular/core';
 import { AdminRoutingModule } from './admin-routing.module';
 import { CartProductModule } from './cart-product/cart-product.module';
 import { CategoryPoductModule } from './category-poduct/category-poduct.module';
-import { DashboardInforComponent } from './dashboard-infor/dashboard-infor.component';
 import { SettingUserModule } from './setting-user/setting-user.module';
 import { ShowAboutModule } from './show-about/show-about.module';
 import { NbCardModule } from '@nebular/theme';
 import { ThemeModule } from '../../@theme/theme.module';
 import { FormsModule } from '@angular/forms';
+import { DashboardInforModule } from './dashboard-infor/dashboard-infor.module';
+import { DashboardInforComponent } from './dashboard-infor/dashboard-infor.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   imports: [
+    CommonModule,
     AdminRoutingModule,
     CartProductModule,
     CategoryPoductModule,
     SettingUserModule,
     ShowAboutModule,
+    DashboardInforModule,
     FormsModule,
     NbCardModule,
     ThemeModule
